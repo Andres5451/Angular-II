92002216ee3d0d841ed19190b38c0905f5d9bcdb
Andres Gomez Buitrago <agomezbuitrago9@gmail.com>
2025-03-21 04:42:44 +0100

    Angular II

---
+++ src/app/components/ejemplo6/ejemplo6.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo6',
+  imports: [],
+  templateUrl: './ejemplo6.component.html',
+  styleUrl: './ejemplo6.component.scss'
+})
+export class Ejemplo6Component {
+
+}
---
+++ src/app/core/menu/menu.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-menu',
+  imports: [Menubar, MenubarModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  ngOnInit(): void {
+    this.items = [
+
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Ejemplo de señales',
+        icon: 'pi pi-star',
+        items: [
+          {
+            label: 'Ejemplo 1',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo1'
+          },
+          {
+            label: 'Ejemplo 2',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo2'
+          },
+          {
+            label: 'Ejemplo 3',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo3'
+          },
+          {
+            label: 'Ejemplo 4',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo4'
+          },
+          {
+            label: 'Ejemplo 5',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo5'
+          },
+          {
+            label: 'Ejemplo 6',
+            icon: 'pi pi-wave-pulse',
+            routerLink: 'ejemplo6'
+          },
+        ]
+      },
+    ]
+  }
+
+}
